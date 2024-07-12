929b675dfa208ad160ea23fcdc74c84fa0dbd888
Drew Millane <112501165+amillane@users.noreply.github.com>
2024-07-12 13:10:56 -0600

    Update gitprofile.config.ts image upload

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -37,7 +37,7 @@ const CONFIG = {
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+            'public/test.jpeg',
           link: 'https://example.com',
         },
         {
