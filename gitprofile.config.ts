ab1c73a7162c70fba292ce7e0b659d4fc51cdb66
Drew Millane <112501165+amillane@users.noreply.github.com>
2024-07-23 07:11:16 -0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -68,7 +68,7 @@ const CONFIG = {
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: '',
+    medium: 'amillane',
     dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
