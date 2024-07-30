90d224a95863c812c6aea168e586bd12a015735a
Drew Millane <112501165+amillane@users.noreply.github.com>
2024-07-30 16:06:34 +0100

    Update index.tsx format 2: image size

--- src/components/blog2-card/index.tsx
+++ src/components/blog2-card/index.tsx
@@ -20,7 +20,7 @@ const BlogCard2 = ({
       array.push(
         <div className="card shadow-lg compact bg-base-100 mb-4" key={index}>
           <div className="p-4 h-full w-full flex items-center">
-            <div className="avatar w-16 h-16 mask mask-squircle mr-4">
+            <div className="avatar w-24 h-24 mask mask-squircle mr-4">
               {skeleton({
                 widthCls: 'w-full',
                 heightCls: 'h-full',
@@ -79,7 +79,7 @@ const BlogCard2 = ({
       >
         <div className="p-4 h-full w-full flex items-center">
           {item.imageUrl && (
-            <div className="avatar w-16 h-16 mask mask-squircle mr-4">
+            <div className="avatar w-24 h-24 mask mask-squircle mr-4">
               <LazyImage
                 src={item.imageUrl}
                 alt={'thumbnail'}
@@ -105,14 +105,14 @@ const BlogCard2 = ({
   return (
     <Fragment>
       <div className="col-span-1 lg:col-span-2">
-        <div className="grid grid-cols-1 gap-6">
+        <div className="grid grid-cols-2 gap-6">
           <div className="col-span-2">
             <div className="card compact bg-base-100 shadow bg-opacity-40">
               <div className="card-body">
-                <div className="mx-3 flex items-center justify-between mb-2">
+                <div className="mx-3 mb-2">
                   <h5 className="card-title">
                     {loading ? (
-                      skeleton({ widthCls: 'w-40', heightCls: 'h-8' })
+                      skeleton({ widthCls: 'w-28', heightCls: 'h-8' })
                     ) : (
                       <span className="text-base-content opacity-70">
                         {header}
