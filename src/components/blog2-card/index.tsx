df0a8a2acd7d3f4f88e7665a8e03a45a443a78df
Drew Millane <112501165+amillane@users.noreply.github.com>
2024-07-30 15:59:27 +0100

    Update index.tsx change format 1

--- src/components/blog2-card/index.tsx
+++ src/components/blog2-card/index.tsx
@@ -18,59 +18,47 @@ const BlogCard2 = ({
     const array = [];
     for (let index = 0; index < externalProjects.length; index++) {
       array.push(
-     <div className="card shadow-lg compact bg-base-100" key={index}>
-          <div className="p-8 h-full w-full">
-            <div className="flex items-center flex-col">
-              <div className="w-full">
-                <div className="flex items-start px-4">
-                  <div className="w-full">
-                    <h2>
-                      {skeleton({
-                        widthCls: 'w-32',
-                        heightCls: 'h-8',
-                        className: 'mb-2 mx-auto',
-                      })}
-                    </h2>
-                    <div className="avatar w-full h-full">
-                      <div className="w-24 h-24 mask mask-squircle mx-auto">
-                        {skeleton({
-                          widthCls: 'w-full',
-                          heightCls: 'h-full',
-                          shape: '',
-                        })}
-                      </div>
-                    </div>
-                    <div className="mt-2">
-                      {skeleton({
-                        widthCls: 'w-full',
-                        heightCls: 'h-4',
-                        className: 'mx-auto',
-                      })}
-                    </div>
-                    <div className="mt-2 flex items-center flex-wrap justify-center">
-                      {skeleton({
-                        widthCls: 'w-full',
-                        heightCls: 'h-4',
-                        className: 'mx-auto',
-                      })}
-                    </div>
-                  </div>
-                </div>
+        <div className="card shadow-lg compact bg-base-100 mb-4" key={index}>
+          <div className="p-4 h-full w-full flex items-center">
+            <div className="avatar w-16 h-16 mask mask-squircle mr-4">
+              {skeleton({
+                widthCls: 'w-full',
+                heightCls: 'h-full',
+                shape: '',
+              })}
+            </div>
+            <div className="w-full">
+              <h2>
+                {skeleton({
+                  widthCls: 'w-32',
+                  heightCls: 'h-8',
+                  className: 'mb-2',
+                })}
+              </h2>
+              <div className="mt-2">
+                {skeleton({
+                  widthCls: 'w-full',
+                  heightCls: 'h-4',
+                })}
+              </div>
+              <div className="mt-2">
+                {skeleton({
+                  widthCls: 'w-full',
+                  heightCls: 'h-4',
+                })}
               </div>
             </div>
           </div>
-        </div>,
+        </div>
       );
     }
-
     return array;
   };
-
 
   const renderExternalProjects = () => {
     return externalProjects.map((item, index) => (
       <a
-        className="card shadow-lg compact bg-base-100 cursor-pointer"
+        className="card shadow-lg compact bg-base-100 mb-4 cursor-pointer"
         key={index}
         href={item.link}
         onClick={(e) => {
@@ -89,35 +77,25 @@ const BlogCard2 = ({
           window?.open(item.link, '_blank');
         }}
       >
-        <div className="p-8 h-full w-full">
-          <div className="flex items-center flex-row">
-            <div className="w-full">
-              <div className="px-4">
-                <div className="text-center w-full">
-                  <h2 className="font-medium text-center opacity-60 mb-2">
-                    {item.title}
-                  </h2>
-                  {item.imageUrl && (
-                    <div className="avatar opacity-90">
-                      <div className="w-24 h-24 mask mask-squircle">
-                        <LazyImage
-                          src={item.imageUrl}
-                          alt={'thumbnail'}
-                          placeholder={skeleton({
-                            widthCls: 'w-full',
-                            heightCls: 'h-full',
-                            shape: '',
-                          })}
-                        />
-                      </div>
-                    </div>
-                  )}
-                  <p className="mt-2 text-base-content text-opacity-60 text-sm text-justify">
-                    {item.description}
-                  </p>
-                </div>
-              </div>
+        <div className="p-4 h-full w-full flex items-center">
+          {item.imageUrl && (
+            <div className="avatar w-16 h-16 mask mask-squircle mr-4">
+              <LazyImage
+                src={item.imageUrl}
+                alt={'thumbnail'}
+                placeholder={skeleton({
+                  widthCls: 'w-full',
+                  heightCls: 'h-full',
+                  shape: '',
+                })}
+              />
             </div>
+          )}
+          <div className="w-full">
+            <h2 className="font-medium opacity-60 mb-2">{item.title}</h2>
+            <p className="text-base-content text-opacity-60 text-sm">
+              {item.description}
+            </p>
           </div>
         </div>
       </a>
@@ -127,7 +105,7 @@ const BlogCard2 = ({
   return (
     <Fragment>
       <div className="col-span-1 lg:col-span-2">
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 gap-6">
           <div className="col-span-2">
             <div className="card compact bg-base-100 shadow bg-opacity-40">
               <div className="card-body">
@@ -137,13 +115,13 @@ const BlogCard2 = ({
                       skeleton({ widthCls: 'w-40', heightCls: 'h-8' })
                     ) : (
                       <span className="text-base-content opacity-70">
-                          {header}
+                        {header}
                       </span>
                     )}
                   </h5>
                 </div>
                 <div className="col-span-2">
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <div className="grid grid-cols-1 gap-6">
                     {loading ? renderSkeleton() : renderExternalProjects()}
                   </div>
                 </div>
