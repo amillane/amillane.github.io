3e68a757c95ce796fc8848a46bd0be8ee8655772
Drew Millane <112501165+amillane@users.noreply.github.com>
2025-03-06 21:01:17 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -124,7 +124,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1smrDvLgWXjN2U2o_HvBUCjzQ9CPC0Vzm/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1fbJ01yI4kD8D6PUoHFb_XqlckJxxEW-Q/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
    'R',
